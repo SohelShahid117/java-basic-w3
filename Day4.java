f98c219e156bafd6338b9d32d11a0eaa306f9d58
SohelShahid117 <sohelshahid09@gmail.com>
2025-05-03 01:10:31 +0600

    done Java Switch

--- Day4.java
+++ Day4.java
@@ -341,12 +341,59 @@ Operator	Name        Description                                                
         }
 
 //        int myNum = 5;
-
         if (myNum % 2 == 0) {
             System.out.println(myNum + " is even");
         } else {
             System.out.println(myNum + " is odd");
         }
+
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+
+        /*
+        The break Keyword
+When Java reaches a break keyword, it breaks out of the switch block.
+
+This will stop the execution of more code and case testing inside the block.
+
+When a match is found, and the job is done, it's time for a break. There is no need for more testing.
+
+A break can save a lot of execution time because it "ignores" the execution of all the rest of the code in the switch block.
+         */
+        int dayy = 4;
+        switch (dayy) {
+            case 6:
+                System.out.println("Today is Saturday");
+                break;
+            case 7:
+                System.out.println("Today is Sunday");
+                break;
+            default:
+                System.out.println("Looking forward to the Weekend");
+        }
+//The default keyword specifies some code to run if there is no case match:    
 
     }
 
