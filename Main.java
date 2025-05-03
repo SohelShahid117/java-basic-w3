f525f9c6f49d74bb246d22940c133cffbeb3fed8
SohelShahid117 <sohelshahid09@gmail.com>
2025-05-04 01:04:52 +0600

    done java_class_methods

--- Main.java
+++ Main.java
@@ -4,4 +4,9 @@ int a = 75;
 //If you don't want the ability to override existing values, declare the attribute as final:
 final int b =99;
 //The final keyword is called a "modifier"
+
+   public void sound(String sound) {
+        System.out.println("Sound is : " + sound);
+    }
+
 }
--- Second.java
+++ Second.java
@@ -1,11 +1,40 @@
 
 class Second {
 
+    //attributes
     int x = 6;
-
     String fname = "John";
     String lname = "Doe";
     int age = 24;
+
+    //methods 
+    static void myMethod() {
+        System.out.println("Hello World!");
+    }
+
+    /*
+    we created a static method, which means that it can be accessed without creating an object of the class,
+    unlike public, which can only be accessed by objects:
+     */
+    // Static method
+    static void myStaticMethod() {
+        System.out.println("Static methods can be called without creating objects");
+    }
+
+    // Public method
+    public void myPublicMethod() {
+        System.out.println("Public methods must be called by creating objects");
+    }
+
+    // Create a fullThrottle() method
+    public void fullThrottle() {
+        System.out.println("The car is going as fast as it can!");
+    }
+
+    // Create a speed() method and add a parameter
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed is: " + maxSpeed);
+    }
 
     public static void main(String[] args) {
         Main myObj = new Main();
@@ -23,6 +52,19 @@ class Second {
 
         System.out.println("Name: " + myObj2.fname + " " + myObj2.lname);
         System.out.println("Age: " + myObj2.age);
+        myObj2.myMethod();
+        myMethod();
+
+        myStaticMethod();
+        //myPublicMethod(); // error: non-static method myPublicMethod() cannot be referenced from a static context
+        myObj2.myPublicMethod();
+
+        Second myCar = new Second();
+        myCar.fullThrottle();
+        myCar.speed(40);
+        
+          Main myCar2 = new Main();
+          myCar2.sound("pop pop");
 
     }
 }
