6bb885b3c1ccf934d4f2b0accca1635c98a507ba
SohelShahid117 <sohelshahid09@gmail.com>
2025-05-03 13:50:04 +0600

    done ava_arrays

--- Day5.java
+++ Day5.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 /*
 1.Loops
 Loops can execute a block of code as long as a specified condition is reached.
@@ -6,6 +9,19 @@ Loops are handy because they save time, reduce errors, and they make code more r
 2.The Do/While Loop
 The do/while loop is a variant of the while loop. This loop will execute the code block once,
 before checking if the condition is true, then it will repeat the loop as long as the condition is true.
+
+3.Java Break
+You have already seen the break statement used in an earlier chapter of this tutorial. It was used to "jump out" of a switch statement.
+The break statement can also be used to jump out of a loop.
+
+4.Java Continue
+The continue statement breaks one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop.
+
+5.Multidimensional Arrays
+A multidimensional array is an array of arrays.
+Multidimensional arrays are useful when you want to store data as a tabular form, like a table with rows and columns.
+To create a two-dimensional array, add each array within its own set of curly braces:
+
  */
 public class Day5 {
 
@@ -93,17 +109,144 @@ There is also a "for-each" loop, which is used exclusively to loop through eleme
         for (int a = 0; a <= 100; a += 10) {
             System.out.println(a);
         }
-        
-            for (int a = 0; a <= 10; a = a + 2) {
-      System.out.println(a);
-    }
-            
-               int number = 2;
 
-    // Print the multiplication table for the number 2
-    for (int k = 1; k <= 10; k++) {
-      System.out.println(number + " x " + k + " = " + (number * k));
-    }
+        for (int a = 0; a <= 10; a = a + 2) {
+            System.out.println(a);
+        }
+
+        int number = 2;
+
+        // Print the multiplication table for the number 2
+        for (int k = 1; k <= 10; k++) {
+            System.out.println(number + " x " + k + " = " + (number * k));
+        }
+
+        for (int k = 0; k < 10; k++) {
+            if (k == 4) {
+                break;
+            }
+            System.out.println(k);
+        }
+
+        for (int k = 0; k < 10; k++) {
+            if (k == 4) {
+                continue;
+            }
+            System.out.println(k);
+        }
+
+        System.out.println("apply \"brreak\" using while loop:");
+        int k = 0;
+        while (k < 10) {
+            System.out.println(k);
+            k++;
+            if (k == 4) {
+                break;
+            }
+        }
+
+        int m = 0;
+        while (m < 10) {
+            if (m == 4) {
+                //m++;
+                m++;
+                continue;
+            }
+            System.out.println(m);
+            m++;
+        }
+
+        /*
+        Java Arrays:
+        Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+        To declare an array, define the variable type with square brackets:
+        String[] cars;
+         */
+        String[] carss = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(carss[0]);
+        System.out.println(carss.length);
+
+        String[] carsss = {"Volvo", "BMW", "Ford", "Mazda"};
+        carsss[0] = "Opel";
+        carsss[2] = "Premio";
+        System.out.println(Arrays.toString(carsss));
+
+        for (String c : carsss) {
+            System.out.println(c);
+        }
+        System.out.println(carsss.length);
+
+        for (int d = 0; d < carsss.length; d++) {
+            System.out.println(cars[d]);
+        }
+
+        // An array storing different ages
+        int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+
+        float avg, sum = 0;
+
+        // Get the length of the array
+        int length = ages.length;
+
+        // Loop through the elements of the array
+        for (int age : ages) {
+            sum += age;
+        }
+
+        // Calculate the average by dividing the sum by the length
+        avg = sum / length;
+
+        // Print the average
+        System.out.println("The average age is: " + avg);
+
+        int lowestAge = ages[0];
+
+        // Loop through the elements of the ages array to find the lowest age
+        for (int age : ages) {
+            // Check if the current age is smaller than the current 'lowest age'
+            if (age < lowestAge) {
+                // If the smaller age is found, update 'lowest age' with that element
+                lowestAge = age;
+            }
+        }
+
+        // Output the value of the lowest age
+        System.out.println("The lowest age in the array is: " + lowestAge);
+
+        int highestAge = ages[0];
+
+        // Loop through the elements of the ages array to find the lowest age
+        for (int age : ages) {
+            // Check if the current age is smaller than the current 'lowest age'
+            if (age > highestAge) {
+                // If the smaller age is found, update 'lowest age' with that element
+                highestAge = age;
+            }
+        }
+
+        // Output the value of the lowest age
+        System.out.println("The lowest age in the array is: " + highestAge);
+
+        int[][] myNumbers = {{1, 2, 3, 4}, {5, 6, 7}};
+        System.out.println(myNumbers[1][1]);
+
+        myNumbers[1][1] = 9;
+        System.out.println(myNumbers[1][1]); // Outputs 9 instead of 7
+
+        System.out.println("apply for-each loop in 2D array");
+        for (int[] q : myNumbers) {
+            for (int r : q) {
+                System.out.println(r);
+            }
+        }
+        
+        System.out.println("apply forloop in 2D array");
+
+        for (int l = 0; l < myNumbers.length; l++) {
+            for (int n = 0; n < myNumbers[l].length; n++) {
+                System.out.println(myNumbers[l][n]);
+            }
+        }
 
     }
 }
