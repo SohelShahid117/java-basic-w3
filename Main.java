979f1df44a0471ad02f6d8290d900f9b355b6c63
SohelShahid117 <sohelshahid09@gmail.com>
2025-05-04 00:44:06 +0600

    done Java Class Attributes

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
 public class Main{
 int a = 75;
+
+//If you don't want the ability to override existing values, declare the attribute as final:
+final int b =99;
+//The final keyword is called a "modifier"
 }
--- Second.java
+++ Second.java
@@ -1,9 +1,28 @@
 
 class Second {
 
+    int x = 6;
+
+    String fname = "John";
+    String lname = "Doe";
+    int age = 24;
+
     public static void main(String[] args) {
         Main myObj = new Main();
+
         System.out.println(myObj.a);
+        myObj.a = 40; //Modify Attributes
+        System.out.println(myObj.a);
+        System.out.println(myObj.b);
+        //myObj.b = 78;
+        //System.out.println(myObj.b); //err,b is final
+        //System.out.println(myObj.x); //err
+
+        Second myObj2 = new Second();
+        System.out.println(myObj2.x);
+
+        System.out.println("Name: " + myObj2.fname + " " + myObj2.lname);
+        System.out.println("Age: " + myObj2.age);
 
     }
 }
