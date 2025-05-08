bf455d53e8786a4f0d757b98fa2ead99ca03ce2f
SohelShahid117 <sohelshahid09@gmail.com>
2025-05-09 02:00:27 +0600

    done 131 : Problem solving-2 (Abstract class Shape)-4min

---
+++ abstraction/src/main/java/problem_solving/Shape.java
@@ -0,0 +1,16 @@
+package problem_solving;
+
+public abstract class Shape {
+
+    double base, height;
+
+    Shape(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+    
+    abstract void area();
+    
+    //131 : Problem solving-2 (Abstract class Shape)-4min
+
+}
