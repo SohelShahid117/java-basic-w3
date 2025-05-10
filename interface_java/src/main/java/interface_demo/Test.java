909c358449415f3c7d7adf1d47142bf65a3f4a93
SohelShahid117 <sohelshahid09@gmail.com>
2025-05-11 00:09:12 +0600

    done 134 : Difference Between Abstract Class And Interface

--- interface_java/src/main/java/interface_demo/Test.java
+++ interface_java/src/main/java/interface_demo/Test.java
@@ -3,6 +3,13 @@
 2.multiple inheritance is possible in interface
 3.interface e method er body takena,method name take only & sei method er body takbe sub class e .so multiple inheritance is possible
 in interface.
+4.interface er object hoina,class er object ready kora jabe.
+5.interface doesn't contain constructor.
+6.all the methods in interface are abstract.
+7.interface can have only abstract method but in class can have abstract method,non abstract method
+8.interface support multiple inheritance,class support only 1 inheritance,
+9.in interface can only have static & final variable,in class can have all types of variable
+10.interface are fully(100%) abstract where class have partial(0 to 100%) abstract
  */
 package interface_demo;
 
