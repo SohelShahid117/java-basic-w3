1b0ce3a271f66e44e79ce882ae191dd22924292b
SohelShahid117 <sohelshahid09@gmail.com>
2025-05-10 23:53:46 +0600

    done 133 : Why Interface Support Multiple Inheritance ?

---
+++ interface_java/src/main/java/interface_demo/C.java
@@ -0,0 +1,10 @@
+package interface_demo;
+
+public class C implements A, B {
+
+    @Override
+    public void play() {
+        System.out.println("hello from class:C");
+    }
+
+}
--- interface_java/src/main/java/interface_demo/Test.java
+++ interface_java/src/main/java/interface_demo/Test.java
@@ -1,12 +1,21 @@
+/*
+1.multiple inheritance is impossible in class
+2.multiple inheritance is possible in interface
+3.interface e method er body takena,method name take only & sei method er body takbe sub class e .so multiple inheritance is possible
+in interface.
+ */
 package interface_demo;
 
 public class Test {
 
     public static void main(String[] args) {
-        Dog D;
+        //Dog D;
 
         Dog d1 = new Dog();
         d1.eat();
+
+        C myObj = new C();
+        myObj.play();
 
     }
 
